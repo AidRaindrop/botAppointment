961edf3d02c895299bab4f6799e4244e6d72c660
eduard <burak.eduard@mail.ru>
2024-02-17 05:16:52 +0100

    addFunstionHello

---
+++ imports.py
@@ -0,0 +1,10 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types
+from tokens import *
+from aiogram import F
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram.enums import ParseMode
+from aiogram import html
