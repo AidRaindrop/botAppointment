dd98549ccaf3b9c8f5145e3e79349321d4ef564f
eduard <burak.eduard@mail.ru>
2024-02-19 13:47:34 +0100

    addButtonsStartandDeleteFunction

--- main.py
+++ main.py
@@ -9,32 +9,31 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    kb = [
+        [
+            types.KeyboardButton(text="Записаться на прием"),
+            types.KeyboardButton(text="Регистрация стоматологии")
+        ],
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите ваше действие"
+    )
+    await message.answer("Начинаем работу с ботом", reply_markup=keyboard)
 
 async def main():
     await dp.start_polling(bot)
 
 #хэндлер на команду /test1
-@dp.message(Command("test1"))
-async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+
 
 # Хэндлер на команду /test2
-@dp.message(Command("test2"))
-async def cmd_test2(message: types.Message):
-    await message.reply("Test 2")
 
-@dp.message(Command("answer"))
-async def cmd_answer(message: types.Message):
-    await message.answer("Это простой ответ")
 
-@dp.message(Command("reply"))
-async def cmd_reply(message: types.Message):
-    await message.reply('Это ответ с "ответом"')
 
-@dp.message(Command("dice"))
-async def cmd_dice(message: types.Message):
-    await message.answer_dice(emoji="🎲")
+
+
 
 @dp.message(F.text, Command("test"))
 async def any_message(message:Message):
@@ -49,14 +48,14 @@ async def cmd_diceC(message: types.Message, bot: Bot):
     await bot.send_dice(keyChannel, emoji="🎲")
 
 #Экранирование ввод, отправка сообщения, в котором текст заранее не известен
-@dp.message(Command("hello"))
-async def cmd_hello(message: Message):
-    await message.answer(
-        f"Hello, {html.bold(html.quote(message.from_user.full_name))}",
-    )
 
-@dp.message(F.text)
-async def echo_with_time(message: Message):
+#такой же форматированный текст как и тот что рпишел
+# @dp.message(F.text)
+# async def echo_with_time(message: Message):
+#     time_now = datetime.now().strftime('%H:%M')
+#
+#     added_text = html.underline(f"Создано в {time_now}")
+#     await message.answer(f"{message.html_text}\n\n{added_text}", parse_mode="HTML")
 
 if __name__ == "__main__":
     asyncio.run(main())
